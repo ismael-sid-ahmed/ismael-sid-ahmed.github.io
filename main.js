274cb10aa8464237530d7da14e31a8f125c6fcbd
ismael-sid-ahmed <85359060+ismael-sid-ahmed@users.noreply.github.com>
2023-03-21 14:03:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,3 +45,19 @@ $cookiesBannerButton.addEventListener("click", () => {
   setCookie(cookieName, "closed");
   $cookiesBanner.remove();
 });
+
+const form = document.querySelector('form');
+const completePaymentButton = document.querySelector('button#complete-payment');
+
+form.addEventListener('submit', handleFormSubmission);
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  if (form.checkValidity() === false) {
+    // Handle invalid form data.
+  } else {
+    completePaymentButton.textContent = 'Making payment...';
+    completePaymentButton.disabled = 'true';
+    setTimeout(() => {alert('Made payment!');}, 500);
+  }
+}
