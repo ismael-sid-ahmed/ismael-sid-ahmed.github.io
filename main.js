39da061f9b6bbd360c9799713a57a32544428c56
ismael-sid-ahmed <85359060+ismael-sid-ahmed@users.noreply.github.com>
2023-03-21 14:18:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,7 @@
-const getCookie = (name) => {
-  const value = " " + document.cookie;
-  console.log("value", `==${value}==`);
-  const parts = value.split(" " + name + "=");
-  return parts.length < 2 ? undefined : parts.pop().split(";").shift();
-};
-
-const setCookie = function (name, value, expiryDays, domain, path, secure) {
-  const exdate = new Date();
-  exdate.setHours(
-    exdate.getHours() +
-      (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
-  );
-  document.cookie =
-    name +
-    "=" +
-    value +
-    ";expires=" +
-    exdate.toUTCString() +
-    ";path=" +
-    (path || "/") +
-    (domain ? ";domain=" + domain : "") +
-    (secure ? ";secure" : "");
-};
-
-(() => {
-  const $cookiesBanner = document.querySelector(".cookies-eu-banner");
-  const $cookiesBannerButton = $cookiesBanner.querySelector("button");
-
-  $cookiesBannerButton.addEventListener("click", () => {
-    $cookiesBanner.remove();
-  });
-})();
-
-const $cookiesBanner = document.querySelector(".cookies-eu-banner");
-const $cookiesBannerButton = $cookiesBanner.querySelector("button");
-const cookieName = "cookiesBanner";
-const hasCookie = getCookie(cookieName);
-
-if (!hasCookie) {
-  $cookiesBanner.classList.remove("hidden");
-}
-
-$cookiesBannerButton.addEventListener("click", () => {
-  setCookie(cookieName, "closed");
-  $cookiesBanner.remove();
-});
-
 const form = document.querySelector('form');
 const completePaymentButton = document.querySelector('button#complete-payment');
 
 form.addEventListener('submit', handleFormSubmission);
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  if (form.checkValidity() === false) {
-    // Handle invalid form data.
-  } else {
-    completePaymentButton.textContent = 'Making payment...';
-    completePaymentButton.disabled = 'true';
-    setTimeout(() => {alert('Made payment!');}, 500);
-  }
-}
 
 const form = document.querySelector('form');
 const completePaymentButton = document.querySelector('button#complete-payment');
