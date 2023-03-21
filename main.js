fa08a63f88bebb5f645fe8b7d9383c1373a778f0
ismael-sid-ahmed <85359060+ismael-sid-ahmed@users.noreply.github.com>
2023-03-21 17:47:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
+
 const form = document.querySelector('form');
 const completePaymentButton = document.querySelector('button#complete-payment');
 
